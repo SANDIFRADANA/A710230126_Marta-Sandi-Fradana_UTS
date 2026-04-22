23990251740c49987a8ccaa084e44ebc17940f37
SANDIFRADANA <sfradana619@gmail.com>
2026-04-22 13:28:58 +0700

    Normalize line endings

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    use AuthorizesRequests;
+
+    public function index()
+    {
+        $chirps = Chirp::with('user')
+            ->latest()
+            ->take(50)
+            ->get();
+
+        return view('home', [
+            'chirps' => $chirps,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        auth()->user()
+            ->chirps()
+            ->create($validated);
+
+        return redirect('/')
+            ->with('success', 'Your chirp has been posted!');
+    }
+
+    public function edit(Chirp $chirp)
+    {
+        $this->authorize('update', $chirp);
+
+        return view('chirps.edit', compact('chirp'));
+    }
+
+    public function update(Request $request, Chirp $chirp)
+    {
+        $this->authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect('/')
+            ->with('success', 'Chirp updated!');
+    }
+
+    public function destroy(Chirp $chirp)
+    {
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect('/')
+            ->with('success', 'Chirp deleted!');
+    }
+}
