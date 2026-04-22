fd1ce9206b881de7ef3d198b4b340a1c036453b8
SANDIFRADANA <sfradana619@gmail.com>
2026-04-22 13:50:13 +0700

    Normalize line endings

---
+++ storage/framework/views/9d4d83fd14105c1c5d02830d641ee14e.php
@@ -0,0 +1,50 @@
+<?php if (isset($component)) { $__componentOriginal23a33f287873b564aaf305a1526eada4 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal23a33f287873b564aaf305a1526eada4 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.layout','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+     <?php $__env->slot('title', null, []); ?> 
+        Welcome
+     <?php $__env->endSlot(); ?>
+
+    <div class="max-w-2xl mx-auto">
+        <?php $__empty_1 = true; $__currentLoopData = $chirps; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $chirp): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                        <div class="font-semibold">
+                            <?php echo e($chirp->user ? $chirp->user->name : 'Anonymous'); ?>
+
+                        </div>
+                        <div class="mt-1">
+                            <?php echo e($chirp->message); ?>
+
+                        </div>
+                        <div class="text-sm text-gray-500 mt-2">
+                            <?php echo e($chirp->created_at->diffForHumans()); ?>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); if ($__empty_1): ?>
+            <p class="text-gray-500 mt-4">
+                No chirps yet. Be the first to chirp!
+            </p>
+        <?php endif; ?>
+    </div>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal23a33f287873b564aaf305a1526eada4)): ?>
+<?php $attributes = $__attributesOriginal23a33f287873b564aaf305a1526eada4; ?>
+<?php unset($__attributesOriginal23a33f287873b564aaf305a1526eada4); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal23a33f287873b564aaf305a1526eada4)): ?>
+<?php $component = $__componentOriginal23a33f287873b564aaf305a1526eada4; ?>
+<?php unset($__componentOriginal23a33f287873b564aaf305a1526eada4); ?>
+<?php endif; ?><?php /**PATH C:\Users\ACER\Herd\chirper\resources\views/home.blade.php ENDPATH**/ ?>
